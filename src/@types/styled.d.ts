071d563c013cb2949ae229a599b2007be7404cfa
Thiago Verissimo da Silva Campos <thiagovsc96@gmail.com>
2024-01-25 15:10:57 +0000

    tipagem de temas, styled que esqueci'

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -2,3 +2,8 @@ import "styled-components";
 import { defaultTheme } from "../styles/themes/default";
 
 type ThemeType = defaultTheme;
+
+// toda vez que eu importar o styled em algum component ele vai pegar a tipagem daqui. aqui é como usar o que já tem lá e sobreescrever o q for preciso
+declare module "styled-components" {
+  export interface DefaultTheme extends ThemeType {}
+}
