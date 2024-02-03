d85a0b4ccefaef94e380f027e67561901a8aa547
Thiago Verissimo da Silva Campos <thiagovsc96@gmail.com>
2024-02-03 11:36:16 +0000

    cores tema, eslint

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,9 +1,9 @@
-import "styled-components";
-import { defaultTheme } from "../styles/themes/default";
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
 
-type ThemeType = defaultTheme;
+type ThemeType = typeof defaultTheme
 
 // toda vez que eu importar o styled em algum component ele vai pegar a tipagem daqui. aqui é como usar o que já tem lá e sobreescrever o q for preciso
-declare module "styled-components" {
+declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,23 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
 *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-
-    body{
-        background: #333;
-        color: #fff;
-    }
 }
-`;
+:focus{
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) =>
+      props.theme['green-500']}; // 0 x 0 y 0 de blur 2px de tamanho
+}
+body{
+    background: ${(props) => props.theme['gray-900']};
+    color: ${(props) => props.theme['gray-300']};
+}
+body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+}
+`
